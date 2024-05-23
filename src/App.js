4a941581621cef0f94c7f9a995c9e774e8308d8f
FarukhBabar <167446651+FarukhBabar@users.noreply.github.com>
2024-05-23 22:07:24 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Pages/Home'
+import Department from './Components/Pages/Department'
+import FoodandDrink from './Components/Pages/FoodandDrink'
+import Food from './Components/Pages/Food'
+import Drinks from './Components/Pages/Drinks'
+import Smoking from './Components/Pages/Smoking'
+import Lighter from './Components/Pages/Lighter'
+import Electrical from './Components/Pages/Electrical'
+import Essentional from './Components/Pages/Essentional'
+import Poundline from './Components/Pages/Poundline'
+import Bathroombeauty from './Components/Pages/Bathroombeauty'
+import Firstaidpharmacy from './Components/Pages/Firstaidpharmacy'
+import Electric from './Components/Pages/Electric'
+import Diytools from './Components/Pages/Diytools'
+import Partybin from './Components/Pages/Partybin'
+import Craft from './Components/Pages/Craft'
+import Carproduct from './Components/Pages/Carproduct'
+import Candels from './Components/Pages/Candels'
+import Foods from './Components/Pages/Foods'
+import Supersunday from './Components/Pages/Supersunday'
+import Palletdeals from './Components/Pages/Palletdeals'
+import Shopbybrand from './Components/Pages/Shopbybrand'
+import Newarrivals from './Components/Pages/Newarrivals'
+import Giftcard from './Components/Pages/Giftcard'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/deparments' element={<Department/>}/>
+        <Route path='/foodanddrinks' element={<FoodandDrink/>}/>
+        <Route path='/foods' element={<Food/>}/>
+        <Route path='/drinks' element={<Drinks/>}/>
+        <Route path='/smoking' element={<Smoking/>}/>
+        <Route path='/lighter' element={<Lighter/>}/>
+        <Route path='/electrical' element={<Electrical/>}/>
+        <Route path='/essentional' element={<Essentional/>}/>
+        <Route path='/poundline' element={<Poundline/>}/>
+        <Route path='/bathroombeauty' element={<Bathroombeauty/>}/>
+        <Route path='/First Aid Pharmacy Personal' element={<Firstaidpharmacy/>}/>
+        <Route path='/Electric' element={<Electric/>}/>
+        <Route path='/Diy' element={<Diytools/>}/>
+        <Route path='/Disposables Party Bin Liners' element={<Partybin/>} />
+        <Route path='/Craft' element={<Craft/>} />
+        <Route path='/Carproduct' element={<Carproduct/>} />
+        <Route path='/Candles Air Freshner' element={<Candels/>} />
+        <Route path='/Food' element={<Foods/>} />
+        <Route path='/Supersunday' element={<Supersunday/>} />
+        <Route path='/Palletdeals' element={<Palletdeals/>} />
+        <Route path='/Shopbybrand' element={<Shopbybrand/>} />
+        <Route path='/Newarrivals' element={<Newarrivals/>} />
+        <Route path='/Giftmakercard' element={<Giftcard/>} />
+        <Route path='/Brighthomly' element={<Giftcard/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Pages/Newarrivals.js
@@ -0,0 +1,318 @@
+import React from 'react'
+import Footer from '../Footer'
+
+const Newarrivals = () => {
+  return (
+    <div>
+         <div>
+  <h2 className="newarrival">New Arrivals</h2>
+  <p className="newartext">Browse Wholesale New Arrivals from the UK’s largest independent Wholesaler. This section is updated weekly with brand new <br /> trusted
+    products, including seasonal ranges. <br /><br />Keep up to date with our latest products as you never know what profitable products you may find…</p>
+  <hr />
+  <hr />
+
+  <div>
+    <div className="d-flex ">
+      <div className="ms-2 sidebar"><h2 className="newar">Shop By</h2>
+        <h2 className="newarr">STOCK STATUS </h2>
+        <p className="mew">IN STOCK <input type="checkbox" id="subscribe" name="subscribe" defaultValue="yes" className="ms-5" /></p> 
+        <p className="mew">OUT OF STOCK  <input type="checkbox" id="subscribe" name="subscribe" defaultValue="yes" className="ms-2" /></p>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className="butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className="butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className="butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="d-flex">
+      <div className="ms-2"><h2 className="newarrr" />
+        <h2 className="newarrr" />
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12  d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="d-flex">
+      <div className="ms-2"><h2 className="newarrr" />
+        <h2 className="newarrr" />
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12  d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="d-flex">
+      <div className="ms-2"><h2 className="newarrr" />
+        <h2 className="newarrr" />
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12  d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/New arrival.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="d-flex">
+      <div className="ms-2"><h2 className="newarrr" />
+        <h2 className="newarrr" />
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12  d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 83.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 74.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 73.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 72.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="d-flex">
+      <div className="ms-2"><h2 className="newarrr" />
+        <h2 className="newarrr" />
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12  d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 71.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 70.png" className="card-img-top card-titl" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 81.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-12 d-flex">
+            <div className="card card12">
+              <img src="./Images/Group 80.png" className="card-img-top card-titll" alt />
+              <div className="card-body">
+                <h2 className="pack">BETTINA TEA TOWEL <br /> COTTON 3 PACK</h2>
+                <h6 className="code"><b>Code:</b> 150550</h6>
+                <button className=" butt">Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<Footer/>
+    </div>
+  )
+}
+
+export default Newarrivals
