60071a125e794e6654284483daed641e44f359c9
FarukhBabar <fahadawan32085346@gmail.com>
2024-05-25 15:51:19 +0500

    setting

--- src/Components/Pages/Adminside/Sundayoffer/Sundayoffer.js
+++ src/Components/Pages/Adminside/Sundayoffer/Sundayoffer.js
@@ -6,8 +6,8 @@ const Sundaypage = () => {
   return (
     <div>
        <div className="container ban">
-  <div className="row">
-    <div className="col-lg-12 col-md-12 col-sm-12">
+  <div className="row ban">
+    <div className="col-lg-12 col-md-12 col-sm-12 ban">
       <img src="https://www.efghousewares.co.uk/Content/14/BannerImage/cb9f5906e2c143c3aa724abfe9ca9cf5.jpg" className="sunday" alt />
     </div>
   </div>
