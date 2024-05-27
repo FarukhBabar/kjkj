ac991bda9d178ea0866594b46e62f58f865f90c0
FarukhBabar <fahadawan32085346@gmail.com>
2024-05-27 10:42:22 +0500

    cards

--- src/Components/Pages/Adminside/Sundayoffer/Card.js
+++ src/Components/Pages/Adminside/Sundayoffer/Card.js
@@ -6,24 +6,28 @@ const Card = ({ user }) => {
     <div className="col-6 col-sm-2 col-md-4 col-lg-3 mt-4">
       <div className="card11212 ">
         <h5 className='image'>
+       
           {/* <Link to={`/freedel/${user._id}`} className="btn"> */}
             {user.image && (
+              <a href=''>
               <img
                 src={`http://localhost:8001/${user.image}`}
                 alt={user.name}
                 className="img-fluid border"
-                style={{ height: '120px', width: '200px' }}
-              />
+                style={{ height: '60%', width: '99%' }}
+              /> </a>
             )}
           {/* </Link> */}
+         
         </h5>
-        <h5 className='ms-1'>{user.name}</h5>
-        <p className='ms-1'>{user.title.slice(0, 25)}...</p>
-        <center>
-          <div className="d-flex">
-            <h5>${user.price}</h5>
+        <h5 className='ms-2'>{user.name.slice(0, 25)}</h5>
+        <p className='ms-2'>{user.title.slice(0, 35)}...</p>
+        {/* <div className="d-flex">
+          <h6>${user.price}</h6>
             <small className="text-danger">(36% off)</small>
-          </div>
+          </div> */}
+        <center>
+          <button className='btn btn-success mb-2 w-100'>Shop now</button>
         </center>
       </div>
     </div>
