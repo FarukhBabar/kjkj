118535549d8c17e3a3105af0ffd82dfd89d1a264
FarukhBabar <fahadawan32085346@gmail.com>
2024-05-28 11:29:00 +0500

    nv

--- src/Components/Pages/Adminside/Adminside.js
+++ src/Components/Pages/Adminside/Adminside.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import "./admin.css"
+import Adminnavbar from './Adminnavbar';
 
 const Listofproducts = () => {
     const [user, setUsers] = useState([]);
@@ -84,122 +85,8 @@ const Listofproducts = () => {
 
 
     </div> */}
-   <div>
-  <nav className="navbar navbar-expand-lg navbar-dark" >
-    <div className="container-fluid">
-      <a className="navbar-brand fw-bold" href="#"></a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/adminsunday">SundayOffer</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link" to="/listpal">Palletdeals</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link" to="/listarr">New Arrivals</Link></li>
-        
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-             Department
-            </a>
-            <ul className="dropdown-menu">
-              {/* <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li> */}
-              <li>
-               
-              </li>
-              <li className="nav-item dropend">
-                <a className="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                 FoodandDrink
-                </a>
-                <ul className="dropdown-menu">
-                  <li><Link className="dropdown-item" to="/listfood">Foods</Link></li>
-                  <li><Link className="dropdown-item" to="/listdrink">Drinks</Link></li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="nav-item dropend">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li>
-                        <hr className="dropdown-divider" />
-                      </li>
-                      <li className="nav-item dropend">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown
-                        </a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Action</a></li>
-                          <li><a className="dropdown-item" href="#">Another action</a></li>
-                          <li>
-                            <hr className="dropdown-divider" />
-                          </li>
-                          <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropend">
-                <a className="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                 Smoking
-                </a>
-                <ul className="dropdown-menu">
-                  <li><Link className="dropdown-item" to="/listlighter">Lighter & Matches</Link></li>
-                  <li><Link className="dropdown-item" to="/listdrink">Drinks</Link></li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="nav-item dropend">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li>
-                        <hr className="dropdown-divider" />
-                      </li>
-                      <li className="nav-item dropend">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown
-                        </a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Action</a></li>
-                          <li><a className="dropdown-item" href="#">Another action</a></li>
-                          <li>
-                            <hr className="dropdown-divider" />
-                          </li>
-                          <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Link</a>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-light" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-</div>
+  
+  <Adminnavbar/>
 
     
    
